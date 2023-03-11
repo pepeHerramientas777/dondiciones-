84377f7016bd2b75b8c1ab5a0692102a721c9763
pepeHerramientas777 <127609105+pepeHerramientas777@users.noreply.github.com>
2023-03-11 08:53:53 -0600

    Add files via upload

---
+++ array.js
@@ -0,0 +1,37 @@
+/*let cuenta=[5, 7, 3, 4]
+document.write(cuenta[3])
+for(let i of cuenta){
+   document.write(i)
+}
+let compras=['casa', 'mesa']
+console.log(compras);
+compras[0]='tahini';
+console.log(compras)*/
+/*  let palabra1 = prompt("deme su primera palabra")
+let palabra2 = prompt("deme su segunda palabra")
+let palabra3 = prompt("deme su tercera palabra")
+let cuenta=['casa', 'oso', 'pepe', 'miel', 'pistola', 'agua', 'sope', 'quesadilla', 'gato', 'perro']
+
+cuenta[2]=palabra1
+
+cuenta[5]=palabra2
+
+cuenta[7]=palabra3
+document.write(cuenta);*/
+/*let palabra1 = prompt("deme su primera palabra")
+let palabra2 = prompt("deme su segunda palabra")
+let palabra3 = prompt("deme su terdera palabra")
+let palabra4 = prompt("deme su cuarta palabra")
+let palabra5 = prompt("deme su quinta palabra")
+let arreglo = [palabra1, palabra2, palabra3, palabra4, palabra5]
+
+arreglo[0] = palabra1
+arreglo[1] = palabra2
+arreglo[2] = palabra3
+arreglo[3] = palabra4
+arreglo[5] = palabra5
+let array = prompt("que pocicion quieres ver?")
+document.write(arreglo[array])*/
+let array=['dato','dato1',]
+array.push('dato3')
+document.write(array)
